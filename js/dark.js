7f5371c39c40592a8581c9b5e52ccd4162a6f6c2
Achraf Khamri <achrafkhamri99@gmail.com>
2020-09-07 11:25:33 +0100

    The underground v1.1.0

---
+++ js/dark.js
@@ -0,0 +1,29 @@
+function darkMode() {
+    var rows = document.querySelectorAll('body');
+    rows.forEach(item => item.classList.toggle("dark-mode"));
+
+    var cards = document.querySelectorAll('.card');
+    cards.forEach(item => item.classList.toggle("dark-card"));
+
+    var search = document.querySelectorAll('.search-bar');
+    search.forEach(item => item.classList.toggle("dark-search-bar"));
+
+    nav = document.getElementsByClassName('navbar');
+    nav[0].classList.toggle('dark-nav');
+    
+    document.getElementsByClassName('my-float')[0].style.transform = 'rotate(180deg)';
+
+
+    if (document.querySelector('.theme').id === 'light') {
+        document.getElementById("logo").src = "./img/underground_logo_light.png";
+        document.getElementsByClassName('float')[0].style.backgroundColor = 'white';
+        document.getElementsByClassName('float')[0].style.color = 'black';
+        document.querySelector('.theme').id = 'dark';
+    } else {
+        document.querySelector('.theme').id = 'light';
+        document.getElementsByClassName('float')[0].style.backgroundColor = '#2C2C2C';
+        document.getElementsByClassName('float')[0].style.color = 'white';
+        document.getElementById("logo").src = "./img/underground_logo.png";
+    }
+    
+}
---
+++ js/main.js
@@ -0,0 +1,51 @@
+// EXAMINE THE DOCUMENT OBJECT //
+
+//console.dir(document);
+//console.log(document.title);
+//document.title="what is this";
+//console.log(document.all);
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
+//headerTitle.textContent='Hello';
+//headerTitle.innerText='Goodbye';	
+//header.style.borderBottom= 'solid 3px black';
+/* var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[0].innerHTML = '<span>This is a list element</span>';
+
+for (let i=0 ; i<items.length;i++){
+	items[i].style.backgroundColor = 'gray';
+} */
+
+
+// QUERYSELECTOR //
+/* var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = 'SEND'
+
+
+
+var items = document.querySelectorAll('.list-group-item');
+items.forEach(function(element) {
+ element.style.backgroundColor='#ccc';
+});
+ */
+
+ // create a div
+ /* var newDiv  = document.createElement('a');
+ newDiv.className='btn btn-lg';
+ newDiv.id='hello';
+ newDiv.setAttribute('href','./index.html');
+ newDiv.innerHTML = '<h1>Test</h1>';
+ console.log(newDiv);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1)
+ */
