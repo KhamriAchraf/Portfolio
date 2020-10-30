60d2d353cb61e1752c7bd3d04c4817acf8dda3a4
Achraf Khamri <achrafkhamri99@gmail.com>
2020-10-30 21:12:21 +0100

    Portfolio

--- js/main.js
+++ js/main.js
@@ -1,51 +1,21 @@
-// EXAMINE THE DOCUMENT OBJECT //
+let slide = $("#webslides");
 
-//console.dir(document);
-//console.log(document.title);
-//document.title="what is this";
-//console.log(document.all);
-//var headerTitle = document.getElementById('header-title');
-//var header = document.getElementById('main-header');
-//headerTitle.textContent='Hello';
-//headerTitle.innerText='Goodbye';	
-//header.style.borderBottom= 'solid 3px black';
-/* var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-items[0].innerHTML = '<span>This is a list element</span>';
-
-for (let i=0 ; i<items.length;i++){
-	items[i].style.backgroundColor = 'gray';
-} */
-
-
-// QUERYSELECTOR //
-/* var header = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4px #ccc';
-
-var input = document.querySelector('input');
-input.value = 'Hello World';
-
-var submit = document.querySelector('input[type="submit"]');
-submit.value = 'SEND'
-
-
-
-var items = document.querySelectorAll('.list-group-item');
-items.forEach(function(element) {
- element.style.backgroundColor='#ccc';
-});
- */
-
- // create a div
- /* var newDiv  = document.createElement('a');
- newDiv.className='btn btn-lg';
- newDiv.id='hello';
- newDiv.setAttribute('href','./index.html');
- newDiv.innerHTML = '<h1>Test</h1>';
- console.log(newDiv);
-
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-
-container.insertBefore(newDiv,h1)
- */
+      document.addEventListener('swiped-up', function (e) {
+          let max = Math.trunc(slide[0].scrollHeight);
+          let offset = Math.trunc(slide[0].offsetHeight);
+          let position = Math.trunc(slide.scrollTop());
+      
+          if ((max - offset == position)) {
+              console.warn("next");
+              window.ws.goNext();
+          }
+      });
+      
+      document.addEventListener('swiped-down', function (e) {
+          let position = Math.trunc(slide.scrollTop());
+      
+          if (position == 0) {
+              console.warn("back");
+              window.ws.goPrev();
+          }
+      });
